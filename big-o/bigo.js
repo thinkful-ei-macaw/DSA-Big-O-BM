565216e4456733a4c1259735731aaedb288b4de7
Brannen Petit <bpetit940@gmail.com>
2020-05-05 13:07:11 -0500

    finished drills

--- big-o/bigo.js
+++ big-o/bigo.js
@@ -19,7 +19,7 @@ function isEven(value) {
 // this algorithm is O(1) constant because it is only returing booleans
 
 function areYouHere(arr1, arr2) {
-    for (let i = 0; i < arr1.length; i++) {
+    for (let i = 0; i < arr1.length; i++) { // nested arrays
         const el1 = arr1[i];
         for (let j = 0; j < arr2.length; j++) {
             const el2 = arr2[j];
@@ -29,4 +29,98 @@ function areYouHere(arr1, arr2) {
     return false;
 }
 
-//
+// this algorithm is utilizing nested loops, so this big O is O(n^2)
+
+function doubleArrayValues(array) {
+    for (let i = 0; i < array.length; i++) {  // O(n)linear
+        array[i] *= 2; // O(1) constant
+    }
+    return array;
+}
+
+// this algorithm utilizes one for loop making it an O(n)linear 
+
+function naiveSearch(array, item) {
+    for (let i = 0; i < array.length; i++) { // O(n)linear
+        if (array[i] === item) {
+            return i;
+        }
+    }
+}
+
+// this algorithm utilizes one for loop making it an O(n)linear 
+
+function createPairs(arr) {
+    for (let i = 0; i < arr.length; i++) {    //nested loops
+        for(let j = i + 1; j < arr.length; j++) {
+            console.log(arr[i] + ", " +  arr[j] );
+        }
+    }
+}
+
+// due to using nested for loops this algorithm is an O(n^2)
+
+function compute(num) {
+    let result = [];
+    for (let i = 1; i <= num; i++) { // linear O(n) 
+
+        if (i === 1) {
+            result.push(0);
+        }
+        else if (i === 2) {
+            result.push(1);
+        }
+        else {
+            result.push(result[i - 2] + result[i - 3]);
+        }
+    }
+    return result;
+}
+
+// linear O(n) 
+
+function efficientSearch(array, item) {
+    let minIndex = 0;
+    let maxIndex = array.length - 1;
+    let currentIndex;
+    let currentElement;
+
+    while (minIndex <= maxIndex) {  // linear O(n) 
+        currentIndex = Math.floor((minIndex + maxIndex) / 2);
+        currentElement = array[currentIndex];
+
+        if (currentElement < item) {
+            minIndex = currentIndex + 1;
+        }
+        else if (currentElement > item) {
+            maxIndex = currentIndex - 1;
+        }
+        else {
+            return currentIndex;
+        }
+    }
+    return -1;
+}
+
+// linear O(n) 
+
+function findRandomElement(arr) {
+    return arr[Math.floor(Math.random() * arr.length)]; // O(1) constant
+
+}
+
+// returns only 1 number
+
+function isWhat(n) {
+    if (n < 2 || n % 1 !== 0) {
+        return false;
+    }
+    for (let i = 2; i < n; ++i) {  
+// linear O(n) 
+        if (n % i === 0) return false;
+    }
+    return true;
+}
+
+
+// linear O(n) uses 1 for loop
